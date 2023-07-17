a75467d2cd8736b9df531bfbe0b017dd182c8eb5
Hakim Saoudi <saoudihakim@gmai.com>
2023-07-17 10:25:30 +0200

    Initial commit

---
+++ src/main/java/com/saoudi/learnspring/AppGamingBasic.java
@@ -0,0 +1,2 @@
+package com.saoudi.learnspring;public class AppGamingBasic {
+}
---
+++ src/test/java/com/saoudi/learnspring/LearnSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saoudi.learnspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
